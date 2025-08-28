6f6b4e7deb5d3b15757d28b58b8dd6bce9a23a95
batosgi <novak34291@gmail.com>
2025-08-28 17:30:07 +0300

    27. Remove Element

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,10 @@
-fn main() {
-    let strs = vec!["a".to_string(), "ab".to_string()];
-    println!("{}", longest_common_prefix(strs));
-}
+fn main() {}
 
-pub fn longest_common_prefix(strs: Vec<String>) -> String {
-    let mut strs_iter = strs.iter();
-    let mut prefix = strs_iter.next().unwrap().as_str();
-
-    for string in strs_iter {
-        if prefix.is_empty() {
-            break;
-        }
-
-        let (mut prefix_chrs, mut string_chrs) = (prefix.chars(), string.chars());
-        for index in 0..prefix.len().max(string.len()) {
-            let (prefix_chr_opt, string_chr_opt) = (prefix_chrs.next(), string_chrs.next());
-
-            match (prefix_chr_opt, string_chr_opt) {
-                (Some(prefix_chr), Some(string_chr)) => {
-                    if prefix_chr != string_chr {
-                        prefix = &prefix[..index];
-                        break;
-                    }
-                    continue;
-                }
-                (Some(_), None) => {
-                    prefix = &prefix[..index];
-                    break;
-                }
-                _ => {
-                    break;
-                }
-            }
+pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
+    for i in (0..nums.len()).rev() {
+        if nums[i] == val {
+            nums.remove(i);
         }
     }
-
-    prefix.to_owned()
+    nums.len() as i32
 }
