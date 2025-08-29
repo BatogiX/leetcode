7c18223b87c1a355a5522a84e873d2ea6a7bd4f7
batosgi <novak34291@gmail.com>
2025-08-29 12:11:38 +0300

    35. Search Insert Position

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,40 @@ fn main() {
     assert_eq!(2, search_insert(vec![1, 3, 5, 6], 5));
     assert_eq!(1, search_insert(vec![1, 3, 5, 6], 2));
     assert_eq!(4, search_insert(vec![1, 3, 5, 6], 7));
+    assert_eq!(4, search_insert(vec![1, 3, 5, 6, 8], 7));
     assert_eq!(6, search_insert(vec![1, 3, 5, 6, 8, 11, 123, 123], 100));
     assert_eq!(0, search_insert(vec![1, 3, 5, 6], 0));
     assert_eq!(8, search_insert(vec![1, 3, 5, 6, 8, 11, 123, 123], 1000));
+    assert_eq!(6, search_insert(vec![1, 3, 5, 6, 8, 11, 123, 124], 123));
 }
 
+#[must_use]
 pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
-    let mut left = 0;
+    use std::cmp::Ordering;
+
+    let mut left = 0i32;
     let mut right = nums.len() as i32 - 1;
-    let mut answer = nums.len() as i32;
 
     while left <= right {
         let mid = left + (right - left) / 2;
 
-        if target == nums[mid as usize] {
-            return mid;
-        }
-
-        if target < nums[mid as usize] {
-            right = mid - 1;
-            answer = mid;
-        } else {
-            left = mid + 1
+        match target.cmp(&nums[mid as usize]) {
+            Ordering::Less => right = mid - 1,
+            Ordering::Equal => return mid,
+            Ordering::Greater => left = mid + 1,
         }
     }
 
-    answer
+    if right < 0 {
+        right = 0;
+    }
+
+    match target.cmp(&nums[right as usize]) {
+        Ordering::Less => match right {
+            0 => right,
+            _ => right - 1,
+        },
+        Ordering::Equal => right,
+        Ordering::Greater => right + 1,
+    }
 }
