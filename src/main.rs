3b5ccea7d536256e4688e1c7d8723f6e2338d94c
batosgi <novak34291@gmail.com>
2025-08-29 20:13:31 +0300

    66. Plus One

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,21 @@
 fn main() {
-    assert_eq!(2, search_insert(vec![1, 3, 5, 6], 5));
-    assert_eq!(1, search_insert(vec![1, 3, 5, 6], 2));
-    assert_eq!(4, search_insert(vec![1, 3, 5, 6], 7));
-    assert_eq!(4, search_insert(vec![1, 3, 5, 6, 8], 7));
-    assert_eq!(6, search_insert(vec![1, 3, 5, 6, 8, 11, 123, 123], 100));
-    assert_eq!(0, search_insert(vec![1, 3, 5, 6], 0));
-    assert_eq!(8, search_insert(vec![1, 3, 5, 6, 8, 11, 123, 123], 1000));
-    assert_eq!(6, search_insert(vec![1, 3, 5, 6, 8, 11, 123, 124], 123));
+    assert_eq!(vec![1, 2, 4], plus_one(vec![1, 2, 3]));
+    assert_eq!(vec![4, 3, 2, 2], plus_one(vec![4, 3, 2, 1]));
+    assert_eq!(vec![1, 0], plus_one(vec![9]));
 }
 
 #[must_use]
-pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
-    use std::cmp::Ordering;
-
-    let mut left = 0i32;
-    let mut right = nums.len() as i32 - 1;
-
-    while left <= right {
-        let mid = left + (right - left) / 2;
-
-        match target.cmp(&nums[mid as usize]) {
-            Ordering::Less => right = mid - 1,
-            Ordering::Equal => return mid,
-            Ordering::Greater => left = mid + 1,
+pub fn plus_one(digits: Vec<i32>) -> Vec<i32> {
+    let mut answer = digits.clone();
+    for (i, digit) in digits.into_iter().enumerate().rev() {
+        if digit == 9 {
+            answer[i] = 0;
+        } else {
+            answer[i] += 1;
+            return answer;
         }
     }
 
-    if right < 0 {
-        right = 0;
-    }
-
-    match target.cmp(&nums[right as usize]) {
-        Ordering::Less => match right {
-            0 => right,
-            _ => right - 1,
-        },
-        Ordering::Equal => right,
-        Ordering::Greater => right + 1,
-    }
+    answer.insert(0, 1);
+    answer
 }
