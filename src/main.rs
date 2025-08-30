305305939ca8b7ce39e94fe97afad6a5bd02f4fb
batosgi <novak34291@gmail.com>
2025-08-31 02:41:21 +0300

    119. Pascal's Triangle II

--- src/main.rs
+++ src/main.rs
@@ -2,42 +2,36 @@ pub struct Solution;
 
 impl Solution {
     #[must_use]
-    pub fn generate(num_rows: i32) -> Vec<Vec<i32>> {
+    pub fn get_row(row_index: i32) -> Vec<i32> {
+        if row_index == 0 {
+            return vec![1];
+        }
+
         let mut triangle = vec![vec![1]];
-
-        if num_rows == 1 {
-            return triangle;
+        for num_row in 1..row_index {
+            triangle.push(Self::create_row(&triangle, num_row as usize));
         }
 
-        for num_row in 2..=num_rows {
-            let mut row = Vec::with_capacity(num_row as usize);
-            row.push(1);
-            row.push(1);
+        Self::create_row(&triangle, row_index as usize)
+    }
 
-            for index in 1..num_row - 1 {
-                let prev1 = triangle[num_row as usize - 2][index as usize - 1];
-                let prev2 = triangle[num_row as usize - 2][index as usize];
-                row.insert(index as usize, prev1 + prev2);
-            }
+    fn create_row(triangle: &[Vec<i32>], num_row: usize) -> Vec<i32> {
+        let mut row = Vec::with_capacity(num_row);
+        row.push(1);
+        row.push(1);
 
-            triangle.push(row);
+        for index in 0..num_row - 1 {
+            let prev1 = triangle[num_row - 1][index];
+            let prev2 = triangle[num_row - 1][index + 1];
+            row.insert(index + 1, prev1 + prev2);
         }
 
-        triangle
+        row
     }
 }
 
 fn main() {
-    assert_eq!(
-        vec![
-            vec![1],
-            vec![1, 1],
-            vec![1, 2, 1],
-            vec![1, 3, 3, 1],
-            vec![1, 4, 6, 4, 1]
-        ],
-        Solution::generate(5)
-    );
-
-    assert_eq!(vec![vec![1]], Solution::generate(1));
+    assert_eq!(vec![1, 3, 3, 1], Solution::get_row(3));
+    assert_eq!(vec![1], Solution::get_row(0));
+    assert_eq!(vec![1, 1], Solution::get_row(1));
 }
