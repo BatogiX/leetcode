dd69f7cc597fcea56e476b995bf4c01208ba8a91
batosgi <novak34291@gmail.com>
2025-08-30 22:24:37 +0300

    108. Convert Sorted Array to Binary Search Tree. Recursive

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,60 @@
-fn main() {
-    let mut nums1 = vec![1, 2, 3, 0, 0, 0];
-    let mut nums2 = vec![2, 5, 6];
-    merge(&mut nums1, 3, &mut nums2, 3);
-    assert_eq!(vec![1, 2, 2, 3, 5, 6], nums1);
+use std::{cell::RefCell, rc::Rc};
 
-    let mut nums1 = vec![1];
-    let mut nums2 = vec![];
-    merge(&mut nums1, 1, &mut nums2, 0);
-    assert_eq!(vec![1], nums1);
-
-    let mut nums1 = vec![0];
-    let mut nums2 = vec![1];
-    merge(&mut nums1, 0, &mut nums2, 1);
-    assert_eq!(vec![1], nums1);
-
-    let mut nums1 = vec![2, 0];
-    let mut nums2 = vec![1];
-    merge(&mut nums1, 1, &mut nums2, 1);
-    assert_eq!(vec![1, 2], nums1);
-
-    let mut nums1 = vec![-1, 0, 0, 3, 3, 3, 0, 0, 0];
-    let mut nums2 = vec![1, 2, 2];
-    merge(&mut nums1, 6, &mut nums2, 3);
-    assert_eq!(vec![-1, 0, 0, 1, 2, 2, 3, 3, 3], nums1);
-
-    let mut nums1 = vec![-1, -1, 0, 0, 0, 0];
-    let mut nums2 = vec![-1, 0];
-    merge(&mut nums1, 4, &mut nums2, 2);
-    assert_eq!(vec![-1, -1, -1, 0, 0, 0], nums1);
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
 }
 
-pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
-    // Removing trailing zeros
-    for _ in 0..nums1.len() - m as usize {
-        nums1.remove(nums1.len() - 1);
-    }
-
-    if nums1.is_empty() {
-        for num2 in nums2.drain(..) {
-            nums1.push(num2);
-        }
-        return;
-    }
-
-    let mut nums1_index = m - 1;
-    let mut nums2_index = n - 1;
-
-    // Merging
-    while nums1_index >= 0 && nums2_index >= 0 {
-        let num1 = nums1[nums1_index as usize];
-        let num2 = nums2[nums2_index as usize];
-
-        if num2 >= num1 {
-            nums2.remove(nums2_index as usize);
-            nums1.insert(nums1_index as usize + 1, num2);
-            nums2_index -= 1;
-        } else {
-            nums1_index -= 1;
+impl TreeNode {
+    #[inline]
+    #[must_use]
+    pub const fn new(val: i32) -> Self {
+        Self {
+            val,
+            left: None,
+            right: None,
         }
     }
+}
 
-    if !nums2.is_empty() {
-        for num2 in nums2.drain(..).rev() {
-            nums1.insert(0, num2);
-        }
+fn main() {
+    let bst = Some(Rc::new(RefCell::new(TreeNode {
+        val: 0,
+        left: Some(Rc::new(RefCell::new(TreeNode {
+            val: -3,
+            left: Some(Rc::new(RefCell::new(TreeNode::new(-10)))),
+            right: None,
+        }))),
+        right: Some(Rc::new(RefCell::new(TreeNode {
+            val: 9,
+            left: Some(Rc::new(RefCell::new(TreeNode::new(5)))),
+            right: None,
+        }))),
+    })));
+    assert_eq!(bst, sorted_array_to_bst(vec![-10, -3, 0, 5, 9]));
+
+    let bst = Some(Rc::new(RefCell::new(TreeNode {
+        val: 3,
+        left: Some(Rc::new(RefCell::new(TreeNode::new(1)))),
+        right: None,
+    })));
+    assert_eq!(bst, sorted_array_to_bst(vec![1, 3]));
+}
+
+#[must_use]
+pub fn sorted_array_to_bst(nums: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
+    if nums.is_empty() {
+        return None;
     }
+
+    let mid = nums.len() / 2;
+    let bst = Rc::new(RefCell::new(TreeNode {
+        val: nums[mid],
+        left: sorted_array_to_bst(nums[..mid].to_owned()),
+        right: sorted_array_to_bst(nums[mid + 1..].to_owned()),
+    }));
+
+    Some(bst)
 }
