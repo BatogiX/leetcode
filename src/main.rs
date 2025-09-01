7c22c7ab2d0a860db1e7ab05ca64f31c4c9be6a7
batosgi <novak34291@gmail.com>
2025-09-01 17:04:03 +0300

    169. Majority Element

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,34 @@ pub struct Solution;
 
 impl Solution {
     #[must_use]
-    pub fn single_number(nums: Vec<i32>) -> i32 {
-        let mut single_num = nums[0];
+    pub fn majority_element(nums: Vec<i32>) -> i32 {
+        let mut nums = nums;
+        nums.sort_unstable();
 
-        for num in &nums[1..] {
-            single_num ^= num;
+        let majority = nums.len() / 2;
+
+        let mut count = 1;
+        let mut prev = nums[0];
+        for next in &nums[1..] {
+            if *next == prev {
+                count += 1;
+            } else {
+                prev = *next;
+                count = 1;
+                continue;
+            }
+
+            if count > majority {
+                return *next;
+            }
         }
 
-        single_num
+        prev
     }
 }
 
 fn main() {
-    assert_eq!(1, Solution::single_number(vec![2, 2, 1]));
-    assert_eq!(4, Solution::single_number(vec![4, 1, 2, 1, 2]));
-    assert_eq!(1, Solution::single_number(vec![1]));
+    assert_eq!(3, Solution::majority_element(vec![3, 2, 3]));
+    assert_eq!(2, Solution::majority_element(vec![2, 2, 1, 1, 1, 2, 2]));
+    assert_eq!(1, Solution::majority_element(vec![1]));
 }
