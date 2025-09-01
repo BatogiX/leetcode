0765babfbbce29c0142e13cb59011a224ac774ff
batosgi <novak34291@gmail.com>
2025-09-01 15:54:03 +0300

    121. Best Time to Buy and Sell Stock

--- src/main.rs
+++ src/main.rs
@@ -2,36 +2,31 @@ pub struct Solution;
 
 impl Solution {
     #[must_use]
-    pub fn get_row(row_index: i32) -> Vec<i32> {
-        if row_index == 0 {
-            return vec![1];
+    pub fn max_profit(prices: Vec<i32>) -> i32 {
+        let mut min_price = prices[0];
+        let mut max_profit = 0;
+
+        for price in &prices[1..] {
+            if *price < min_price {
+                min_price = *price;
+                continue;
+            }
+
+            let profit = price - min_price;
+            if profit > max_profit {
+                max_profit = profit;
+            }
         }
 
-        let mut triangle = vec![vec![1]];
-        for num_row in 1..row_index {
-            triangle.push(Self::create_row(&triangle, num_row as usize));
-        }
-
-        Self::create_row(&triangle, row_index as usize)
-    }
-
-    fn create_row(triangle: &[Vec<i32>], num_row: usize) -> Vec<i32> {
-        let mut row = Vec::with_capacity(num_row);
-        row.push(1);
-        row.push(1);
-
-        for index in 0..num_row - 1 {
-            let prev1 = triangle[num_row - 1][index];
-            let prev2 = triangle[num_row - 1][index + 1];
-            row.insert(index + 1, prev1 + prev2);
-        }
-
-        row
+        max_profit
     }
 }
 
 fn main() {
-    assert_eq!(vec![1, 3, 3, 1], Solution::get_row(3));
-    assert_eq!(vec![1], Solution::get_row(0));
-    assert_eq!(vec![1, 1], Solution::get_row(1));
+    assert_eq!(5, Solution::max_profit(vec![7, 1, 5, 3, 6, 4]));
+    assert_eq!(0, Solution::max_profit(vec![7, 6, 4, 3, 1]));
+    assert_eq!(12, Solution::max_profit(vec![8, 20, 4, 5]));
+    assert_eq!(0, Solution::max_profit(vec![1]));
+    assert_eq!(9, Solution::max_profit(vec![1, 10]));
+    assert_eq!(3, Solution::max_profit(vec![1, 10]));
 }
