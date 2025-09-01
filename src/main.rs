b1ec9455df620d779da2bde438624f76d139696f
batosgi <novak34291@gmail.com>
2025-09-01 17:55:43 +0300

    217. Contains Duplicate

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,19 @@
 pub struct Solution;
 
+use std::collections::HashSet;
+
 impl Solution {
     #[must_use]
-    pub fn majority_element(nums: Vec<i32>) -> i32 {
-        let mut nums = nums;
-        nums.sort_unstable();
-
-        let majority = nums.len() / 2;
-
-        let mut count = 1;
-        let mut prev = nums[0];
-        for next in &nums[1..] {
-            if *next == prev {
-                count += 1;
-            } else {
-                prev = *next;
-                count = 1;
-                continue;
-            }
-
-            if count > majority {
-                return *next;
-            }
-        }
-
-        prev
+    pub fn contains_duplicate(nums: Vec<i32>) -> bool {
+        let mut hs = HashSet::with_capacity(nums.len());
+        !nums.iter().all(|&num| hs.insert(num))
     }
 }
 
 fn main() {
-    assert_eq!(3, Solution::majority_element(vec![3, 2, 3]));
-    assert_eq!(2, Solution::majority_element(vec![2, 2, 1, 1, 1, 2, 2]));
-    assert_eq!(1, Solution::majority_element(vec![1]));
+    assert!(Solution::contains_duplicate(vec![1, 2, 3, 1]));
+    assert!(!Solution::contains_duplicate(vec![1, 2, 3, 4]));
+    assert!(Solution::contains_duplicate(vec![
+        1, 1, 1, 3, 3, 4, 3, 2, 4, 2
+    ]));
 }
