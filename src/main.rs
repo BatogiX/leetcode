9a10c13fa2b1283f8122a8b38c64cdf62800d786
batosgi <novak34291@gmail.com>
2025-09-01 21:51:31 +0300

    219. Contains Duplicate II

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,36 @@
 pub struct Solution;
 
-use std::collections::HashSet;
+use std::collections::{HashMap, hash_map::Entry};
 
 impl Solution {
     #[must_use]
-    pub fn contains_duplicate(nums: Vec<i32>) -> bool {
-        let mut hs = HashSet::with_capacity(nums.len());
-        !nums.iter().all(|&num| hs.insert(num))
+    pub fn contains_nearby_duplicate(nums: Vec<i32>, k: i32) -> bool {
+        let mut hm = HashMap::with_capacity(nums.len());
+
+        for (index, num) in nums.iter().enumerate() {
+            match hm.entry(num) {
+                Entry::Occupied(mut occupied_entry) => {
+                    if index.abs_diff(*occupied_entry.get()) <= k as usize {
+                        return true;
+                    }
+
+                    occupied_entry.insert(index);
+                }
+                Entry::Vacant(vacant_entry) => {
+                    vacant_entry.insert(index);
+                }
+            }
+        }
+
+        false
     }
 }
 
 fn main() {
-    assert!(Solution::contains_duplicate(vec![1, 2, 3, 1]));
-    assert!(!Solution::contains_duplicate(vec![1, 2, 3, 4]));
-    assert!(Solution::contains_duplicate(vec![
-        1, 1, 1, 3, 3, 4, 3, 2, 4, 2
-    ]));
+    assert!(Solution::contains_nearby_duplicate(vec![1, 2, 3, 1], 3));
+    assert!(Solution::contains_nearby_duplicate(vec![1, 0, 1, 1], 1));
+    assert!(!Solution::contains_nearby_duplicate(
+        vec![1, 2, 3, 1, 2, 3],
+        2
+    ));
 }
