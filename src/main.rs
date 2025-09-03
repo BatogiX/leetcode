db22c822952a1ee0d59287a05be693b1eee81b6b
batosgi <novak34291@gmail.com>
2025-09-03 21:42:36 +0300

    303. Range Sum Query - Immutable

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,28 @@
-pub struct Solution;
+struct NumArray {
+    nums: Vec<i32>,
+    pre_sum: Vec<i32>,
+}
 
-impl Solution {
-    pub fn move_zeroes(nums: &mut Vec<i32>) {
-        let mut j = 0;
+impl NumArray {
+    fn new(nums: Vec<i32>) -> Self {
+        let mut pre_sum: Vec<i32> = vec![0; nums.len()];
+        pre_sum[0] = nums[0];
 
-        for i in 0..nums.len() {
-            if nums[i] != 0 {
-                nums[j] = nums[i];
-                j += 1;
-            }
+        for i in 1..nums.len() {
+            pre_sum[i] = pre_sum[i - 1] + nums[i];
         }
 
-        for i in j..nums.len() {
-            nums[i] = 0;
-        }
+        Self { nums, pre_sum }
+    }
+
+    fn sum_range(&self, left: i32, right: i32) -> i32 {
+        return self.pre_sum[right as usize] - self.pre_sum[left as usize] + self.nums[left as usize];
     }
 }
 
 fn main() {
-    let mut nums = vec![0, 1, 0, 3, 12];
-    Solution::move_zeroes(&mut nums);
-    assert_eq!(vec![1, 3, 12, 0, 0], nums);
-
-    let mut nums = vec![0];
-    Solution::move_zeroes(&mut nums);
-    assert_eq!(vec![0], nums);
-
-    let mut nums = vec![0, 0, 1];
-    Solution::move_zeroes(&mut nums);
-    assert_eq!(vec![1, 0, 0], nums);
+    let obj = NumArray::new(vec![-2, 0, 3, -5, 2, -1]);
+    assert_eq!(1, obj.sum_range(0, 2));
+    assert_eq!(-1, obj.sum_range(2, 5));
+    assert_eq!(-3, obj.sum_range(0, 5));
 }
