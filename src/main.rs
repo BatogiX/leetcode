b468f835b00e7f4e986360da25469740934c3753
batosgi <novak34291@gmail.com>
2025-09-06 20:09:00 +0300

    448. Find All Numbers Disappeared in an Array

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,46 @@
 struct Solution;
 
 impl Solution {
-    pub fn third_max(mut nums: Vec<i32>) -> i32 {
-        let len = nums.len();
-
-        if len == 1 {
-            return nums[0];
-        }
-
-        if len == 2 {
-            return nums[0].max(nums[1]);
-        }
-
-        nums.sort_unstable();
-        let mut nums_iter_rev = nums.iter().rev();
-        let mut prev = nums_iter_rev.next().unwrap();
-        let mut count = 1;
-
-        for num in nums_iter_rev {
-            if num != prev {
-                if count == 2 {
-                    return *num;
-                }
-                count += 1;
-                prev = num;
+    pub fn find_disappeared_numbers(mut nums: Vec<i32>) -> Vec<i32> {
+        for i in 0..nums.len() {
+            let index = (nums[i].abs() - 1) as usize;
+            if nums[index] > 0 {
+                nums[index] = -nums[index];
             }
         }
 
-        nums[len - 1]
+        let mut disappeared_numbers = vec![];
+        for (index, &num) in nums.iter().enumerate() {
+            if num > 0 {
+                disappeared_numbers.push((index + 1) as i32);
+            }
+        }
+
+        disappeared_numbers
     }
 }
 
 fn main() {
-    assert_eq!(1, Solution::third_max(vec![3, 2, 1]));
-    assert_eq!(2, Solution::third_max(vec![1, 2]));
-    assert_eq!(1, Solution::third_max(vec![2, 2, 3, 1]));
-    assert_eq!(2, Solution::third_max(vec![1, 1, 2]));
+    assert_eq!(
+        vec![5, 6],
+        Solution::find_disappeared_numbers(vec![4, 3, 2, 7, 8, 2, 3, 1])
+    );
+    assert_eq!(
+        vec![5, 6, 9],
+        Solution::find_disappeared_numbers(vec![4, 3, 2, 7, 8, 8, 2, 3, 1])
+    );
+    assert_eq!(
+        vec![5, 6, 9, 10],
+        Solution::find_disappeared_numbers(vec![4, 3, 2, 7, 8, 8, 8, 2, 3, 1])
+    );
+    assert_eq!(vec![2], Solution::find_disappeared_numbers(vec![1, 1]));
+    assert_eq!(vec![1], Solution::find_disappeared_numbers(vec![2, 2]));
+    assert_eq!(
+        Vec::<i32>::new(),
+        Solution::find_disappeared_numbers(vec![1, 2])
+    );
+    assert_eq!(
+        vec![1, 3],
+        Solution::find_disappeared_numbers(vec![2, 2, 2])
+    );
 }
