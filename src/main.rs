937fc43a6a4c8ac902d8c4d9cda532db92a660b9
batosgi <novak34291@gmail.com>
2025-09-07 21:07:55 +0300

    485. Max Consecutive Ones

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,45 @@
 struct Solution;
 
 impl Solution {
-    pub fn island_perimeter(grid: Vec<Vec<i32>>) -> i32 {
-        let rows_len = grid.len() as i32;
-        let columns_len = grid[0].len() as i32;
-        let mut total_perimeter = 0;
+    pub fn find_max_consecutive_ones(nums: Vec<i32>) -> i32 {
+        let mut max_consecutive_ones = 0;
+        let mut consecutive_ones = 0;
 
-        for (y, row) in grid.iter().enumerate() {
-            for (x, cell) in row.iter().enumerate() {
-                if cell == &1 {
-                    for cord in [-1, 1] {
-                        let mut adj = x as i32 + cord;
-                        if adj == -1 || adj == columns_len || grid[y][adj as usize] == 0 {
-                            total_perimeter += 1;
-                        }
-
-                        adj = y as i32 + cord;
-                        if adj == -1 || adj == rows_len || grid[adj as usize][x] == 0 {
-                            total_perimeter += 1;
-                        }
-                    }
+        for num in nums {
+            if num == 1 {
+                consecutive_ones += 1;
+            } else {
+                if consecutive_ones > max_consecutive_ones {
+                    max_consecutive_ones = consecutive_ones;
                 }
+
+                consecutive_ones = 0;
             }
         }
 
-        total_perimeter
+        if consecutive_ones > max_consecutive_ones {
+            max_consecutive_ones = consecutive_ones;
+        }
+
+        max_consecutive_ones
     }
 }
 
 fn main() {
     assert_eq!(
-        16,
-        Solution::island_perimeter(vec![
-            vec![0, 1, 0, 0],
-            vec![1, 1, 1, 0],
-            vec![0, 1, 0, 0],
-            vec![1, 1, 0, 0]
-        ])
+        Solution::find_max_consecutive_ones(vec![1, 1, 0, 1, 1, 1]),
+        3
     );
 
-    assert_eq!(4, Solution::island_perimeter(vec![vec![1]]));
-    assert_eq!(4, Solution::island_perimeter(vec![vec![1, 0]]));
+    assert_eq!(
+        Solution::find_max_consecutive_ones(vec![1, 0, 1, 1, 0, 1]),
+        2
+    );
+
+    assert_eq!(Solution::find_max_consecutive_ones(vec![1]), 1);
+    assert_eq!(Solution::find_max_consecutive_ones(vec![0]), 0);
+    assert_eq!(
+        Solution::find_max_consecutive_ones(vec![1, 0, 1, 1, 0, 1]),
+        2
+    );
 }
