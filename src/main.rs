ac1d4cbda76f538fc123225edc8306214c1868de
batosgi <novak34291@gmail.com>
2025-09-17 16:55:17 +0300

    506. Relative Ranks

--- src/main.rs
+++ src/main.rs
@@ -3,40 +3,35 @@ struct Solution;
 use std::collections::HashMap;
 
 impl Solution {
-    pub fn find_words(words: Vec<String>) -> Vec<String> {
-        let keyboard = {
-            let mut keyboard = HashMap::new();
-            let rows = [
-                vec!['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
-                vec!['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
-                vec!['z', 'x', 'c', 'v', 'b', 'n', 'm'],
-            ];
-
-            for (index, row) in rows.into_iter().enumerate() {
-                for char in row {
-                    keyboard.insert(char, index as i8);
-                    keyboard.insert(char.to_ascii_uppercase(), index as i8);
-                }
+    pub fn find_relative_ranks(mut scores: Vec<i32>) -> Vec<String> {
+        let mut ranks = {
+            let mut ranks = Vec::with_capacity(scores.len());
+            ranks.push("Gold Medal".to_owned());
+            if scores.len() >= 2 {
+                ranks.push("Silver Medal".to_owned());
             }
-
-            keyboard
+            if scores.len() >= 3 {
+                ranks.push("Bronze Medal".to_owned());
+            }
+            for index in 3..scores.len() {
+                ranks.push((index + 1).to_string());
+            }
+            ranks
         };
 
-        let mut answer = vec![];
-        'outer: for word in words {
-            let mut word_chars = word.chars();
-            let mut previous_row_number = keyboard[&word_chars.next().unwrap()];
-
-            for char in word_chars {
-                let row_number = keyboard[&char];
-
-                if row_number != previous_row_number {
-                    continue 'outer;
-                }
-                previous_row_number = row_number;
+        let hm = {
+            let mut hm = HashMap::with_capacity(scores.len());
+            for (index, score) in scores.iter().enumerate() {
+                hm.insert(*score, index);
             }
+            hm
+        };
 
-            answer.push(word);
+        scores.sort_unstable();
+
+        let mut answer = vec![String::with_capacity(0); scores.len()];
+        for score in scores {
+            answer[hm[&score]] = ranks.pop().unwrap();
         }
 
         answer
@@ -44,29 +39,16 @@ impl Solution {
 }
 
 fn main() {
-    println!("a: {}, A: {}", 'a' as u32, 'A' as u32);
     assert_eq!(
-        Solution::find_words(vec![
-            "Hello".to_owned(),
-            "Alaska".to_owned(),
-            "Dad".to_owned(),
-            "Peace".to_owned()
-        ]),
-        vec!["Alaska", "Dad"]
+        Solution::find_relative_ranks(vec![5, 4, 3, 2, 1]),
+        vec!["Gold Medal", "Silver Medal", "Bronze Medal", "4", "5"]
     );
 
     assert_eq!(
-        Solution::find_words(vec!["omk".to_owned()]),
-        Vec::<String>::new()
+        Solution::find_relative_ranks(vec![10, 3, 8, 9, 4]),
+        vec!["Gold Medal", "5", "Bronze Medal", "Silver Medal", "4"]
     );
 
-    assert_eq!(
-        Solution::find_words(vec!["adsdf".to_owned(), "sfd".to_owned()]),
-        vec!["adsdf", "sfd"]
-    );
-
-    assert_eq!(
-        Solution::find_words(vec!["a".to_owned(), "b".to_owned()]),
-        vec!["a", "b"]
-    );
+    assert_eq!(Solution::find_relative_ranks(vec![10]), vec!["Gold Medal"]);
+    assert_eq!(Solution::find_relative_ranks(vec![5, 10]), vec!["Silver Medal", "Gold Medal"]);
 }
