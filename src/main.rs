6c8667f509fc1ccae8048c019b6be3e25379c19c
batosgi <novak34291@gmail.com>
2025-10-18 20:08:44 +0300

    575. Distribute Candies

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,17 @@
 struct Solution;
 
+use std::collections::HashSet;
+
 impl Solution {
-    pub fn matrix_reshape(mat: Vec<Vec<i32>>, r: i32, c: i32) -> Vec<Vec<i32>> {
-        let r = r as usize;
-        let c = c as usize;
-
-        if r * c != mat.len() * mat[0].len() {
-            return mat;
-        }
-
-        let flat_mat: Vec<i32> = mat.into_iter().flatten().collect();
-
-        let mut reshaped_mat = Vec::with_capacity(r * c);
-        for chunk in flat_mat.chunks(c) {
-            reshaped_mat.push(Vec::from(chunk));
-        }
-
-        reshaped_mat
+    pub fn distribute_candies(candy_type: Vec<i32>) -> i32 {
+        let max_candies = candy_type.len() / 2;
+        let unique_candy_types = candy_type.into_iter().collect::<HashSet<_>>();
+        max_candies.min(unique_candy_types.len()) as i32
     }
 }
 
 fn main() {
-    assert_eq!(
-        Solution::matrix_reshape(vec![vec![1, 2], vec![3, 4]], 1, 4),
-        vec![vec![1, 2, 3, 4]]
-    );
-
-    assert_eq!(
-        Solution::matrix_reshape(vec![vec![1, 2], vec![3, 4]], 2, 4),
-        vec![vec![1, 2], vec![3, 4]]
-    );
-
-    assert_eq!(
-        Solution::matrix_reshape(vec![vec![1, 2, 3, 4]], 2, 2),
-        vec![vec![1, 2], vec![3, 4]]
-    );
-
-    assert_eq!(
-        Solution::matrix_reshape(vec![vec![1, 2, 3, 4, 5, 6, 7, 8]], 4, 2),
-        vec![vec![1, 2], vec![3, 4], vec![5, 6], vec![7, 8]]
-    );
-
-    assert_eq!(
-        Solution::matrix_reshape(vec![vec![1, 2]], 1, 1),
-        vec![vec![1, 2]]
-    );
+    assert_eq!(Solution::distribute_candies(vec![1, 1, 2, 2, 3, 3]), 3);
+    assert_eq!(Solution::distribute_candies(vec![1, 1, 2, 3]), 2);
+    assert_eq!(Solution::distribute_candies(vec![6, 6, 6, 6]), 1);
 }
